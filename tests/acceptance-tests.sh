4b2d87414c7812b26a524f7219592b48fa5d1a91
Joren Van Onder <joren.vanonder@gmail.com>
2019-01-21 12:04:54 -0800

    Add base pointer addressing acceptance test

--- tests/acceptance-tests.sh
+++ tests/acceptance-tests.sh
@@ -13,13 +13,41 @@ fail () {
     echo "${FILENAME} resulted in ${RESULT}, expected ${EXPECTED}."
 }
 
-cargo build
-target/debug/jvo-asm examples/print.jas
-OUT=$(./a.out)
-EXPECTED="abc"
+compile_and_compare_stdout () {
+    SRC="${1}"
+    EXPECTED="${2}"
 
-if [ "${OUT}" != "${EXPECTED}" ]; then
-    fail 'print.jas' "${OUT}" "${EXPECTED}"
-fi
+    target/debug/jvo-asm "${SRC}"
+
+    # the binary is allowed to return non-zero without stopping the tests
+    set +e
+    OUT=$(./a.out)
+    set -e
+
+    if [ "${OUT}" != "${EXPECTED}" ]; then
+        fail "${SRC}" "${OUT}" "${EXPECTED}"
+    fi
+}
+
+compile_and_compare_return () {
+    SRC="${1}"
+    EXPECTED="${2}"
+
+    target/debug/jvo-asm "${SRC}"
+
+    # the binary is allowed to return non-zero without stopping the tests
+    set +e
+    ./a.out
+    RETURN="${?}"
+    set -e
+
+    if [ "${RETURN}" -ne "${EXPECTED}" ]; then
+        fail "${SRC}" "${RETURN}" "${EXPECTED}"
+    fi
+}
+
+cargo build
+compile_and_compare_stdout 'examples/print.jas' 'abc'
+compile_and_compare_return 'examples/base_ptr_addressing.jas' '4'
 
 exit $FAILED
