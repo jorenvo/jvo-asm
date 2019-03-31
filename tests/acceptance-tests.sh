4057d2a6e8883cd280517e61291954d37ded4cb3
Joren Van Onder <joren.vanonder@gmail.com>
2019-03-31 15:53:57 -0700

    Add square example

--- tests/acceptance-tests.sh
+++ tests/acceptance-tests.sh
@@ -51,5 +51,6 @@ compile_and_compare_stdout 'examples/print.jas' 'abc'
 compile_and_compare_return 'examples/base_ptr_addressing.jas' '4'
 compile_and_compare_return 'examples/multiple_data_sections.jas' '6'
 compile_and_compare_return 'examples/find_max.jas' '222'
+compile_and_compare_return 'examples/square.jas' '49'
 
 exit $FAILED
