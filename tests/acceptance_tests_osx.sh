c5b657268aab9b643deef58ffea0135aaa51e058
Joren Van Onder <joren@jvo.sh>
2020-01-11 18:46:29 -0800

    Rename mach tests

--- tests/acceptance_tests_osx.sh
+++ tests/acceptance_tests_osx.sh
@@ -2,6 +2,6 @@
 set -euo pipefail
 source tests/acceptance_tests_common.sh
 
-compile_and_compare_return 'mach' 'examples/exit.jas' '99'
+compile_and_compare_return 'mach' 'examples/exit_mach.jas' '99'
 
 exit $FAILED
